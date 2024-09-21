644339a736845af43c9dc768888d38d1576160c1
Tomasz Ferens <tomaszferens@hotmail.com>
2024-09-21 12:12:21 +0200

    feat: setup environment variables

---
+++ src/env.ts
@@ -0,0 +1,27 @@
+import { z } from "zod"
+
+const zodEnvSchema = z.object({
+  PORT: z.string(),
+  NODE_ENV: z.string(),
+})
+
+function parseEnv() {
+  try {
+    return zodEnvSchema.parse(process.env)
+  } catch (err) {
+    if (err instanceof z.ZodError) {
+      const { fieldErrors } = err.flatten()
+      const errorMessage = Object.entries(fieldErrors)
+      .map(([field, errors]) =>
+        errors ? `"${field}": ${errors.join(", ")}` : field,
+      )
+      .join("\n  ")
+      throw new Error(
+        `Missing environment variables:\n  ${errorMessage}`,
+      )
+    }
+    throw err;
+  }
+}
+
+export const env = parseEnv()
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,6 @@
-console.log('Hello World!');
+import { app } from "./app";
+import { env } from "./env";
+
+app.listen(env.PORT, () => {
+	console.log(`🚀 Running on Port ${env.PORT}`);
+});
